0cce8fbe7aa374937bc29a63ead42270cc2a95ad
Narayana Swamy, Asha <Asha.Narayana.Swamy.extern@ergo.com>
2024-02-28 15:55:55 +0100

    change daisy theme'

--- app/products/Products.tsx
+++ app/products/Products.tsx
@@ -4,7 +4,9 @@ import AddToCart from "./AddToCart";
 
 export default function Products() {
     return (
-        <div className="p-5 my-5 bg-sky-400 hover:bg-sky-50"><AddToCart/></div>
+        <div className="p-5 my-5 bg-sky-400 hover:bg-sky-50"><AddToCart /></div>
+        
+        
       )
   }
   
